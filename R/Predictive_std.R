c6d1622f30519db40d944410df7b65dfe5961e31
Daniel Gil <danielgils@gmail.com>
2019-04-16 10:09:10 +0200

    Decision tree, RF, Adaboost, XGB, Bayesian NN
    Using Caret

--- R/Predictive_std.R
+++ R/Predictive_std.R
@@ -16,6 +16,7 @@ library(dplyr)
 library(factoextra) # PCA
 library(pROC)
 library(glmnet) # Lasso and Ridge logistic
+library(doParallel)
 
 #' Reading data
 training_std <- read.table("Data/train_sub_std_up.csv", sep = ",", dec = ".", 
@@ -220,3 +221,264 @@ confusionMatrix(data = elastic_pred, reference = testing_std$CHURN,
 (elastic_roc <- roc(testing_std$CHURN, elastic_pred_prob$X1))
 plot(elastic_roc)
 
+#' *Decision tree with complexity as tuning parameter*
+getModelInfo("rpart")
+modelLookup("rpart")
+set.seed(12345)
+tree <- train(CHURN ~ ., data = training_std[,-c(35, 36, 38)], 
+                 method = "rpart", trControl = fitControl, metric = "ROC",
+              tuneLength = 50)
+tree
+
+# ROC value under different pruning values (complexity parameter) 
+trellis.par.set(caretTheme())
+plot(tree) 
+
+# plot the model
+plot(tree$finalModel, uniform = TRUE, main = "Decision Tree (cp)")
+text(tree$finalModel, use.n. = TRUE, all = TRUE, cex = .8)
+
+#' Variable importance
+tree_varimp <- varImp(tree)
+plot(tree_varimp, main = "Variable Importance - Decision Tree (cp)")
+
+#' Prediction on testing dataset
+tree_pred_prob <- predict(tree, testing_std, type = "prob")  # predicted scores
+tree_pred <- predict(tree, testing_std)  # predicted class
+
+# Confusion matrix
+confusionMatrix(data = tree_pred, reference = testing_std$CHURN, 
+                mode = "everything", positive = "X1")
+
+# ROC 
+(tree_roc <- roc(testing_std$CHURN, tree_pred_prob$X1))
+plot(tree_roc)
+
+
+#' *Decision tree with max depth as tuning parameter*
+modelLookup("rpart2")
+set.seed(12345)
+tree2 <- train(CHURN ~ ., data = training_std[,-c(35, 36, 38)], 
+              method = "rpart2", trControl = fitControl, metric = "ROC",
+              tuneLength = 50)
+tree2
+
+# ROC value under different pruning values (complexity parameter) 
+trellis.par.set(caretTheme())
+plot(tree2) 
+
+# plot the model
+plot(tree2$finalModel, uniform = TRUE, main = "Decision Tree (max_depth)")
+text(tree2$finalModel, use.n. = TRUE, all = TRUE, cex = .8)
+
+#' Variable importance
+tree2_varimp <- varImp(tree2)
+plot(tree2_varimp, main = "Variable Importance - Decision Tree (max_depth)")
+
+#' Prediction on testing dataset
+tree2_pred_prob <- predict(tree2, testing_std, type = "prob")  # predicted scores
+tree2_pred <- predict(tree2, testing_std)  # predicted class
+
+# Confusion matrix
+confusionMatrix(data = tree2_pred, reference = testing_std$CHURN, 
+                mode = "everything", positive = "X1")
+
+# ROC 
+(tree2_roc <- roc(testing_std$CHURN, tree2_pred_prob$X1))
+plot(tree2_roc)
+
+#' *Random Forest*
+modelLookup("rf")
+
+cl <- makePSOCKcluster(10)
+registerDoParallel(cl)
+set.seed(12345)
+rf <- train(CHURN ~ ., data = training_std[,-c(35, 36, 38)], 
+               method = "rf", trControl = fitControl, metric = "ROC",
+               tuneLength = 10)
+stopCluster(cl)
+rf
+
+# ROC value under different tuning paramters
+trellis.par.set(caretTheme())
+plot(rf) 
+
+#' Variable importance
+rf_varimp <- varImp(rf)
+plot(rf_varimp, main = "Variable Importance - Random Forest")
+
+#' Prediction on testing dataset
+rf_pred_prob <- predict(rf, testing_std, type = "prob")  # predicted scores
+rf_pred <- predict(rf, testing_std)  # predicted class
+
+# Confusion matrix
+confusionMatrix(data = rf_pred, reference = testing_std$CHURN, 
+                mode = "everything", positive = "X1")
+
+# ROC 
+(rf_roc <- roc(testing_std$CHURN, rf_pred_prob$X1))
+plot(rf_roc)
+
+# Prediction on test set
+rf_pred_test <- predict(rf, test_std, type = "prob")  # predicted scores
+rf_pred_test_out <- cbind(ID = test_std$ID, Churn = rf_pred_test$X1)
+
+write.csv(rf_pred_test_out, "Output/rf.csv", row.names = F)
+
+#' *Random Forest specifying number of variables in each tree*
+modelLookup("rf")
+
+#' Size of subset features
+mtry <- sqrt(ncol(x))
+tunegrid <- expand.grid(.mtry = mtry)
+
+cl <- makePSOCKcluster(10)
+registerDoParallel(cl)
+set.seed(12345)
+rf2 <- train(CHURN ~ ., data = training_std[,-c(35, 36, 38)], 
+            method = "rf", trControl = fitControl, metric = "ROC",
+            tuneGrid = tunegrid)
+stopCluster(cl)
+rf2
+
+# ROC value under different tuning paramters
+#trellis.par.set(caretTheme())
+#plot(rf2) 
+
+#' Variable importance
+rf2_varimp <- varImp(rf2)
+plot(rf2_varimp, main = "Variable Importance - Random Forest (size_var)")
+
+#' Prediction on testing dataset
+rf2_pred_prob <- predict(rf2, testing_std, type = "prob")  # predicted scores
+rf2_pred <- predict(rf2, testing_std)  # predicted class
+
+# Confusion matrix
+confusionMatrix(data = rf2_pred, reference = testing_std$CHURN, 
+                mode = "everything", positive = "X1")
+
+# ROC 
+(rf2_roc <- roc(testing_std$CHURN, rf2_pred_prob$X1))
+plot(rf2_roc)
+
+# Prediction on test set
+rf2_pred_test <- predict(rf2, test_std, type = "prob")  # predicted scores
+rf2_pred_test_out <- cbind(ID = test_std$ID, Churn = rf2_pred_test$X1)
+
+write.csv(rf2_pred_test_out, "Output/rf2.csv", row.names = F)
+
+
+#' *Adaboost*
+modelLookup("adaboost")
+cl <- makePSOCKcluster(10)
+registerDoParallel(cl)
+set.seed(12345)
+ada <- train(CHURN ~ ., data = training_std[,-c(35, 36, 38)], 
+             method = "adaboost", trControl = fitControl, metric = "ROC",
+             tuneLength = 10)
+stopCluster(cl)
+ada
+
+# ROC value under different tuning paramters
+trellis.par.set(caretTheme())
+plot(ada) 
+
+#' Variable importance
+ada_varimp <- varImp(ada)
+plot(ada_varimp, main = "Variable Importance - Adaboost")
+
+#' Prediction on testing dataset
+ada_pred_prob <- predict(ada, testing_std, type = "prob")  # predicted scores
+ada_pred <- predict(ada, testing_std)  # predicted class
+
+# Confusion matrix
+confusionMatrix(data = ada_pred, reference = testing_std$CHURN, 
+                mode = "everything", positive = "X1")
+
+# ROC 
+(ada_roc <- roc(testing_std$CHURN, ada_pred_prob$X1))
+plot(ada_roc)
+
+# Prediction on test set
+ada_pred_test <- predict(ada, test_std, type = "prob")  # predicted scores
+ada_pred_test_out <- cbind(ID = test_std$ID, Churn = ada_pred_test$X1)
+
+write.csv(ada_pred_test_out, "Output/adaboost.csv", row.names = F)
+
+#' *Extreme Gradient Boosting*
+modelLookup("xgbDART")
+cl <- makePSOCKcluster(10)
+registerDoParallel(cl)
+set.seed(12345)
+xgb <- train(CHURN ~ ., data = training_std[,-c(35, 36, 38)], 
+             method = "xgbDART", trControl = fitControl, metric = "ROC",
+             tuneLength = 5, verbose = F)
+stopCluster(cl)
+xgb
+
+# ROC value under different tuning paramters
+trellis.par.set(caretTheme())
+plot(xgb) 
+
+#' Variable importance
+xgb_varimp <- varImp(xgb)
+plot(xgb_varimp, main = "Variable Importance - XGB")
+
+#' Prediction on testing dataset
+xgb_pred_prob <- predict(xgb, testing_std, type = "prob")  # predicted scores
+xgb_pred <- predict(xgb, testing_std)  # predicted class
+
+# Confusion matrix
+confusionMatrix(data = xgb_pred, reference = testing_std$CHURN, 
+                mode = "everything", positive = "X1")
+
+# ROC 
+(xgb_roc <- roc(testing_std$CHURN, xgb_pred_prob$X1))
+plot(xgb_roc)
+
+# Prediction on test set
+xgb_pred_test <- predict(xgb, test_std, type = "prob")  # predicted scores
+xgb_pred_test_out <- cbind(ID = test_std$ID, Churn = xgb_pred_test$X1)
+
+write.csv(xgb_pred_test_out, "Output/XGB.csv", row.names = F)
+
+#' *Bayesian Neural Net*
+modelLookup("brnn")
+cl <- makePSOCKcluster(10)
+registerDoParallel(cl)
+set.seed(12345)
+bnn <- train(CHURN ~ ., data = training_std[,-c(35, 36, 38)], 
+             method = "brnn", trControl = fitControl, metric = "ROC",
+             tuneLength = 5)
+stopCluster(cl)
+bnn
+
+# ROC value under different tuning paramters
+trellis.par.set(caretTheme())
+plot(bnn) 
+
+#' Variable importance
+bnn_varimp <- varImp(bnn)
+plot(bnn_varimp, main = "Variable Importance - Bayesian Neural Net")
+
+#' Prediction on testing dataset
+bnn_pred_prob <- predict(bnn, testing_std, type = "prob")  # predicted scores
+bnn_pred <- predict(bnn, testing_std)  # predicted class
+
+# Confusion matrix
+confusionMatrix(data = bnn_pred, reference = testing_std$CHURN, 
+                mode = "everything", positive = "X1")
+
+# ROC 
+(bnn_roc <- roc(testing_std$CHURN, bnn_pred_prob$X1))
+plot(bnn_roc)
+
+# Prediction on test set
+bnn_pred_test <- predict(bnn, test_std, type = "prob")  # predicted scores
+bnn_pred_test_out <- cbind(ID = test_std$ID, Churn = bnn_pred_test$X1)
+
+write.csv(bnn_pred_test_out, "Output/BayesianNN.csv", row.names = F)
+
+#' Saving environment
+save.image(file = 'Training.RData')
+load('Training.RData')
